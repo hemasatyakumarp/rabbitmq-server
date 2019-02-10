74b7a319bad2c500c72e7cfe51b114a50b82b36b
Dan Shick <d.shick@tcs.com>
2019-02-10 20:21:13 +0000

    Fixed issues

--- rmq-setup.sh
+++ rmq-setup.sh
@@ -8,9 +8,12 @@ wget -O erl.deb https://bit.ly/2E4UUxT
 wget -O rmq.tar.xz https://bit.ly/2SnHcyB
 
 dpkg -x erl.deb ./erl
-cp ./erl/usr/lib/erlang/erts-10.2.3/bin/* ~/.bin/
+cp -R ./erl/usr/lib/erlang ~/.bin/erlang
 
-tar xfv rmq.deb
+tar xfv rmq.tar.xz
 mv rabbitmq_server-3.7.11 ~/.bin/rmq-server
 
 popd
+
+cp ./files/erl ~/.bin/erlang/erts-10.2.3/bin/erl
+chmod +x ~/.bin/erlang/erts-10.2.3/bin/erl
